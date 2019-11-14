2bde939c6c2223c9ea103cfb8d601f0a6a88f4a5
Mihlos <miguel.mish@gmail.com>
2019-11-14 21:40:17 +0100

    listar buckets y files con client y resource

--- myS3.py
+++ myS3.py
@@ -7,13 +7,45 @@ import boto3
 #     print(bucket.name)
 
 class S3:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self):
         self.s3_client = boto3.client('s3')
+        self.s3_resource = boto3.resource('s3')
+        
 
-    def create_bucket(self):
-        self.s3_client.create_bucket(Bucket=self.name, 
+    def create_bucket(self, name):
+        self.s3_client.create_bucket(Bucket=name, 
                                 CreateBucketConfiguration={
                                     'LocationConstraint': 'eu-west-1'
                                     }
                                 )
+
+    def list_buckets_client(self):
+        response = self.s3_client.list_buckets()
+        buckets = [bucket['Name'] for bucket in response['Buckets']]
+        print('List of Buckets ==>')
+        for bucket in buckets:
+            print('Bucket name: {}'.format(bucket))
+
+    def list_buckets_resource(self):
+        buckets = [bucket.name for bucket in self.s3_resource.buckets.all()]
+        print('List of Buckets ==>')
+        for bucket in buckets:
+            print('Bucket name: {}'.format(bucket))
+
+    def list_content_client(self, name):
+        response = self.s3_client.list_objects(Bucket=name)
+        files = [file['Key'] for file in response['Contents']]
+        print('List of Files in {} ==>'.format(name))
+        for file in files:
+            print('File name: {}'.format(file))
+
+    def list_content_resource(self, name):
+        my_bucket = self.s3_resource.Bucket(name)
+        files = [file.key for file in my_bucket.objects.all()]
+        print('List of Files in {} ==>'.format(name))
+        for file in files:
+            print('File name: {}'.format(file))
+
+
+
+    
--- savefile.py
+++ savefile.py
@@ -2,5 +2,9 @@ from myS3 import S3
 
 
 if __name__ == "__main__":
-    s3mihlos = S3('mihlos-bucket-test')
-    s3mihlos.create_bucket()
+    s3mihlos = S3()
+    # s3mihlos.create_bucket('mihlos-bucket-test')
+    # s3mihlos.list_buckets_client()
+    # s3mihlos.list_content_client('mihlos-test2')
+    s3mihlos.list_buckets_resource()
+    s3mihlos.list_content_resource('mihlos-test2')
